9929efa7473705e4aa32d3086116b166b9728550
Eric Robert <3ric.robert@gmail.com>
2017-03-26 20:06:35 -0400

    Rename sqs2js to sqscat

--- cmd/sqscat/main.go
+++ cmd/sqscat/main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -14,74 +15,100 @@ import (
 )
 
 func main() {
+	defer func() {
+		if err := recover(); err != nil {
+			os.Exit(1)
+		}
+	}()
+
 	log.SetFlags(0)
 
-	url := flag.String("url", "", "url of SQS queue")
-	cmd := flag.String("cmd", "", "command to invoke for each message")
-	n := flag.Int("n", 0, "number of messages to process")
+	url := flag.String("url", "", "url of the SQS queue")
+	cmd := flag.String("cmd", "", "command to invoke for each SQS message")
+	n := flag.Int("n", 0, "number of SQS messages to retrieve (0 means unlimited)")
+	t := flag.Duration("t", 0, "time to wait between requests to SQS")
+	batch := flag.Int("batch", 10, "batch size (must be between 1 and 10)")
 	flag.Parse()
 
 	if url == nil || *url == "" || cmd == nil || *cmd == "" {
 		usage()
 	}
 
+	if k := *batch; k < 1 || k > 10 {
+		log.Panicf("batch size %d is not between 1 and 10", k)
+	}
+
 	s, err := session.NewSession()
 	if err != nil {
-		log.Fatalln("session:", err)
+		log.Panicf("session: %s", err)
 	}
 
 	q := &queue{
-		q:    sqs.New(s),
-		n:    10,
-		url:  *url,
-		cmd:  *cmd,
-		args: flag.Args(),
+		q:     sqs.New(s),
+		batch: *batch,
+		n:     int64(*n),
+		t:     *t,
+		url:   *url,
+		cmd:   *cmd,
+		args:  flag.Args(),
 	}
 
 	for {
-		if n != nil {
-			k := int64(*n)
-
-			if q.i == k {
-				break
-			}
-
-			if left := k - q.i; q.n > left {
-				q.n = left
-			}
+		if q.n != 0 && q.i == q.n {
+			break
 		}
 
 		if err = q.read(); err != nil {
 			break
 		}
+
+		if q.i == q.n {
+			break
+		}
+
+		if q.t != 0 {
+			time.Sleep(q.t)
+		}
 	}
 
 	if err != nil {
-		os.Exit(-1)
+		log.Panicf("read: %s", err)
 	}
 }
 
 func usage() {
-	log.Println("sqs2js -url=sqs-url [-n=N] -cmd=prog [args...]")
+	log.Println(os.Args[0], "-url=sqs-url [-n=N] -cmd=prog [args...]")
 	log.Println()
 	flag.PrintDefaults()
 	os.Exit(-1)
 }
 
 type queue struct {
-	q    *sqs.SQS
-	i    int64
-	n    int64
-	url  string
-	cmd  string
-	args []string
+	q     *sqs.SQS
+	batch int
+	i     int64
+	n     int64
+	t     time.Duration
+	url   string
+	cmd   string
+	args  []string
 }
 
 func (q *queue) read() (err error) {
+	j := int64(q.batch)
+
+	if q.n != 0 {
+		j = q.n - q.i
+	}
+
+	if j > 10 {
+		j = 10
+	}
+
 	args := &sqs.ReceiveMessageInput{
 		QueueUrl:            &q.url,
-		MaxNumberOfMessages: &q.n,
-		WaitTimeSeconds:     aws.Int64(20),
+		MaxNumberOfMessages: &j,
+		WaitTimeSeconds:     aws.Int64(20), // using SQS long polling
 	}
 
 	r, err := q.q.ReceiveMessage(args)
@@ -132,7 +159,7 @@ func (q *queue) invoke(p []byte) (err error) {
 	}
 
 	if _, ok := err.(*exec.ExitError); !ok {
-		log.Println(err)
+		log.Panicf("run: %s", err)
 	}
 
 	return
